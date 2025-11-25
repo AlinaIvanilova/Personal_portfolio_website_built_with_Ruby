3832fa782cdcbb0167dc62fb1d09f2804eb0bb53
AlinaIvanilova <alinatelf@gmail.com>
2025-11-25 16:09:26 +0200

    version-three: added page contacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'contact/index'
   # Health check
   get "up" => "rails/health#show", as: :rails_health_check
 
   # About page
   get "about", to: "about#index"
+
+  # contact page
+  get "contact", to: "contact#index"
 
   # Main page - /about
   root "about#index"
