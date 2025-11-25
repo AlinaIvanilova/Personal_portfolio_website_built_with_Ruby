0d62c8d4466049711801e82c25e7abaff2fb894d
AlinaIvanilova <alinatelf@gmail.com>
2025-11-25 18:14:54 +0200

    version-four: added pages skills and projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'projects/index'
+  get 'skills/index'
   get 'contact/index'
   # Health check
   get "up" => "rails/health#show", as: :rails_health_check
@@ -8,6 +10,12 @@ Rails.application.routes.draw do
 
   # contact page
   get "contact", to: "contact#index"
+
+  # skills page
+  get "skills", to: "skills#index"
+
+  # projects page
+  get "projects", to: "projects#index"
 
   # Main page - /about
   root "about#index"
